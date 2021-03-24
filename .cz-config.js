cbb2613119fbd5d93eb1cacc9c5ffba1cff11dff
ljl <776745393@qq.com>
2021-03-24 22:59:03 +0800

    1

--- .cz-config.js
+++ .cz-config.js
@@ -1,18 +1,21 @@
-'use strict';
+'use strict'
 
 module.exports = {
   types: [
-    {value: 'feat',     name: 'feat:     新功能'},
-    {value: 'fix',      name: 'fix:      修复'},
-    {value: 'bug',      name: 'bug:      bug'},
-    {value: 'docs',     name: 'docs:     文档变更'},
-    {value: 'style',    name: 'style:    代码格式(不影响代码运行的变动)'},
-    {value: 'refactor', name: 'refactor: 重构(既不是增加feature，也不是修复bug)'},
-    {value: 'perf',     name: 'perf:     性能优化'},
-    {value: 'test',     name: 'test:     增加测试'},
-    {value: 'chore',    name: 'chore:    构建过程或辅助工具的变动'},
-    {value: 'revert',   name: 'revert:   回退'},
-    {value: 'build',    name: 'build:    打包'}
+    { value: 'feat', name: 'feat:     新功能' },
+    { value: 'fix', name: 'fix:      修复' },
+    // {value: 'bug',      name: 'bug:      bug'},
+    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
+    {
+      value: 'refactor',
+      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+    },
+    { value: 'perf', name: 'perf:     性能优化' },
+    { value: 'test', name: 'test:     增加测试' },
+    { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
+    { value: 'revert', name: 'revert:   回退' },
+    { value: 'build', name: 'build:    打包' }
   ],
   // override the messages, defaults are as follows
   messages: {
@@ -31,4 +34,4 @@ module.exports = {
   skipQuestions: ['body', 'footer'],
   // limit subject length, commitlint默认是72
   subjectLimit: 72
-};
+}
